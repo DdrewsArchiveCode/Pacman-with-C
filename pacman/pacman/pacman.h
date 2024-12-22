a89d480efa4aa068d2c12e98294918aa43d2ee59
Drew.Y <73530267+DdrewsArchiveCode@users.noreply.github.com>
2024-12-23 01:28:23 +1100

    Add files via upload

---
+++ pacman/pacman/pacman.h
@@ -0,0 +1,26 @@
+// Header file for define, and function prototype
+
+// Line 2-13 is provided by Blackbox AI
+#define WIDTH 10
+#define HEIGHT 10
+
+// Define the characters for the board
+#define EMPTY ' '
+#define WALL '#'
+#define DOT '.'
+#define PACMAN 'P'
+
+void initializeBoard (char board[HEIGHT][WIDTH]);
+void printBoard (char board[HEIGHT][WIDTH]);
+
+/* 
+    Starting from this line, all of 
+    them is my own decalration
+*/
+
+// constant
+#define GHOST 'G'
+
+//Function Prototype
+void levelBuild (char board[HEIGHT][WIDTH]);
+void movementMechanic (char board[HEIGHT][WIDTH]);

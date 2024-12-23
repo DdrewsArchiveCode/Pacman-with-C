bad24425529223fedec428562bc7d99d5d25b5ef
Ddrew.Y <73530267+DdrewsArchiveCode@users.noreply.github.com>
2024-12-23 15:09:19 +1100

    Add files via upload

---
+++ pacman/pacman.c
@@ -0,0 +1,16 @@
+/*
+Made by Deandrew.Y (UNSW student ID z5641581)
+Started on 23 December 2024 00:36
+With the help of Blackbox AI for board template
+*/
+
+#include <stdio.h>
+#include "pacman.h"
+
+int main(void) {
+    char board[HEIGHT][WIDTH];
+    initializeBoard(board);
+    levelBuild(board);
+    movementMechanic(board);
+    return 0;
+}
---
+++ pacman/pacmanFunc.c
@@ -0,0 +1,91 @@
+// Files for function that are used for the pacman code
+
+#include <stdio.h>
+#include <ctype.h>
+#include "pacman.h"
+
+// Function to initialize the board
+void initializeBoard (char board[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            if (i == 0 || i == HEIGHT - 1 || j == 0 || j == WIDTH - 1) {
+                board[i][j] = WALL; // Create walls
+            } else {
+                board[i][j] = DOT; // Fill with dot
+            }
+        }
+    }
+}
+
+// Function to print the board
+void printBoard (char board[HEIGHT][WIDTH]) {
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < WIDTH; j++) {
+            printf("%c ", board[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+// Function for user to setup their own level
+void levelBuild (char board[HEIGHT][WIDTH]) {
+    char input = 'c';
+    int coorX = 0;
+    int coorY = 0;
+
+    printf("=== Welcome to level building phase! ===\n");
+    printf("Level Building Dictionary: \n"
+            "w = building a single wall \n"
+            "g = adding a ghost in the board\n");
+
+    printf ("Input the command that you wish: ");
+    scanf(" %c %d %d", &input, &coorX, &coorY);
+    input = tolower(input);
+    
+    while (input != 'q') {
+        input = tolower(input);
+        if (input == 'w') {
+            inputWall(coorX, coorY, board);
+        } else if (input == 'g') {
+            inputGhost(coorX, coorY, board);
+        }
+
+        printBoard(board);
+        printf ("Input the command that you wish: ");
+        scanf(" %c %d %d", &input, &coorX, &coorY);
+        input = tolower(input);
+    }
+}
+
+// Function to compute movement mechanic
+void movementMechanic (char board[HEIGHT][WIDTH]) {
+    char mechanic;
+    int checker = TRUE;
+
+    while (checker = TRUE) {
+        printf("Movement command: ");
+        scanf(" %c", mechanic);
+        mechanic = tolower(mechanic);
+
+        if (mechanic == 'w') {
+            return;
+        } else if (mechanic == 's') {
+            return;
+        } else if (mechanic == 'd') {
+            return;
+        } else if (mechanic == 'a') {
+            return;
+        }
+        checker = winCondition(board);
+    }
+}
+
+// Function that add walls
+void inputWall(int coorX, int coorY, char board[HEIGHT][WIDTH]) {
+    board[coorY][coorX] = WALL;
+}
+
+// Function that add ghost
+void inputGhost(int coorX, int coorY, char board[HEIGHT][WIDTH]) {
+    board[coorY][coorX] = GHOST;
+}
